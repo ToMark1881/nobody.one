b34fd488082d83b4e4dac5880b32b76bdaf767bc
vladyslavvdovychenko <vladmustaine@gmail.com>
2020-07-08 00:07:17 +0300

    youtube and appleMusic done [2]

---
+++ nobodyOne/Models/Song.swift
@@ -0,0 +1,24 @@
+//
+//  Song.swift
+//  nobodyOne
+//
+//  Created by Vladyslav Vdovychenko on 07.07.2020.
+//  Copyright © 2020 Vladyslav Vdovychenko. All rights reserved.
+//
+
+import Foundation
+
+
+class Song {
+    
+    let title: String?
+    let timestamp: Int?
+    let position: Int?
+    
+    init(_ dict: Dictionary<String, AnyObject>) {
+        self.title = dict["title"] as? String
+        self.timestamp = dict["timestamp"] as? Int
+        self.position = dict["position"] as? Int
+    }
+    
+}
